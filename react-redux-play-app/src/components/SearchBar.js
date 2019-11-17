d1c7dcefe27098441d0c7daac5c356ac6f263807
Ryan Johnson <rjohnson4444@live.com>
2019-11-17 05:51:39 -0700

    add all the react-redux things

--- react-redux-play-app/src/components/SearchBar.js
+++ react-redux-play-app/src/components/SearchBar.js
@@ -9,11 +9,18 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
-        <input onChange={event => this.setState({ term: event.target.value })} />
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChage(event.target.value)} />
       </div>
     )
   }
-};
+
+  onInputChage(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
+}
 
 export default SearchBar;
--- react-redux-play-app/src/index.js
+++ react-redux-play-app/src/index.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
-
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 const API_KEY = 'AIzaSyD_EE8UO0GtaBjsXiANDabFq23rUAgc8mw';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container') );;
